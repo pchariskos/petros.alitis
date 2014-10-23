4c22858df41e610069fe1f98a29e2d3b5fd6397a
petroschariskos <pchariskos@gmail.com>
2014-10-24 00:31:03 +0200

    Set up the Theme of the app.Generated class diagram.

--- Alitis/src/petros/alitis/MainActivity.java
+++ Alitis/src/petros/alitis/MainActivity.java
@@ -20,18 +20,16 @@ import android.view.ViewGroup;
 public class MainActivity extends ActionBarActivity implements ActionBar.TabListener {
 
     /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v13.app.FragmentStatePagerAdapter}.
-     */
+	 * The   {@link android.support.v4.view.PagerAdapter}   that will provide fragments for each 
+	 * of the sections. We use a  {@link FragmentPagerAdapter}   derivative, which will keep every
+	 * loaded fragment in memory. If this becomes too memory intensive, it may be best to switch 
+	 * to a  {@link android.support.v13.app.FragmentStatePagerAdapter} .
+	 */
     SectionsPagerAdapter mSectionsPagerAdapter;
 
     /**
-     * The {@link ViewPager} that will host the section contents.
-     */
+	 * The   {@link ViewPager}   that will host the section contents.
+	 */
     ViewPager mViewPager;
 
     @Override
